011cf5a7f1ba24bddf776d116fac5d88f476a211
dimikhan <dimikhan@in.ibm.com>
2016-08-04 13:56:25 +0530

    Update BankDao.java

--- src/com/ibm/bluemix/BankDao.java
+++ src/com/ibm/bluemix/BankDao.java
@@ -14,7 +14,7 @@ public class BankDao {
 
 
 //Sample Comment
-//Some changes from Dimitry
+//Some changes by Dimitry
 public boolean chkLogin(String userName, String passWord) {
 	boolean bool=false;
 	PreparedStatement pst=null;
